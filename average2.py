0bfd57cfd24188cbb7d168d9a544dce68332472c
ishraqalhadi <48090994+ishraqalhadi@users.noreply.github.com>
2019-02-28 12:29:35 +0000

    Add files via upload

---
+++ average2.py
@@ -0,0 +1,12 @@
+print " This program will ask you to type in two numbers."
+print " The program will add them & average them and"
+print " output the results to the screen"
+print 
+num1 = input(" Type in a number: ") 
+print
+num2 = input(" Type in another number: ")
+print
+sum = num1 + num2
+print " The sum of the numbers is ", sum
+average = sum / 2.0
+print " Their average is", average
