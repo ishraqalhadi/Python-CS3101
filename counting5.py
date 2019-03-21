d0161dbb5a6cadb5373b8f15ead6a559b5a2d0a8
ishraqalhadi <48090994+ishraqalhadi@users.noreply.github.com>
2019-03-21 13:15:28 +0000

    Add files via upload

---
+++ counting5.py
@@ -0,0 +1,13 @@
+# Counting5.py
+# a program using a for loop and the range function
+# to display on the screen the numbers 5 to 1 in steps of 1. 
+# D.H. 5th January 2005
+
+print "A count from 5 to 1 in steps of 1 "
+
+# range is called with three arguments
+# a start number, an end number (up to but not included), and the step of -1
+ 
+for i in range(5,0,-1):
+    print i
+
